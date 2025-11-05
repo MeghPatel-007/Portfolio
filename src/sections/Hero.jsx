51ef989f5450619715fdb3f507432549128eef41
MeghPatel-007 <meghpatel0009@gmail.com>
2025-11-05 19:25:16 +0530

    Normalize line endings to LF

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+    return (
+        <section>
+            <h1>Welcome to My Portfolio</h1>
+            <p>Hi, I'm Megh Patel, a passionate web developer.</p>
+        </section>
+    );
+}
+
+export default Hero
